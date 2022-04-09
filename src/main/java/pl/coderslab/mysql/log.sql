04dad46edf5ffc2e6ac689fc237ae13b8533d6cd
spiglowska <sylwiapiglowska@onet.eu>
2022-04-09 23:36:15 +0200

    Create database and table users

---
+++ src/main/java/pl/coderslab/mysql/log.sql
@@ -0,0 +1,12 @@
+CREATE TABLE `users` (
+     `id` int(11) NOT NULL,
+     `username` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+     `email` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+     `password` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+
+
+
+
+
